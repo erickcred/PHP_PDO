9d678a5d51a9732c61d9496522a5180dd5f3333e
Erick Rick <erickcredjes@gmail.com>
2021-01-12 13:29:13 -0300

    Criando a index

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php 
+require_once "view/layout/header.php";
+
+require_once "./model/Servico.php";
+require_once "./db/DB.php";
+require_once "./db/Dao/ServicoDao.php";
+
+?>
+
+
+
+<?php 
+require_once 'view/layout/footer.php';
